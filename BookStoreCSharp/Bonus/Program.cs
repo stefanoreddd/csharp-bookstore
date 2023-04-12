a9f036011308d8d73bbe853eed96bfd531fcec22
stefanoreddd <stefanogg95@gmail.com>
2023-04-12 18:27:31 +0200

    Aggiungere i file di progetto.

---
+++ BookStoreCSharp/Bonus/Program.cs
@@ -0,0 +1,84 @@
+﻿Console.Write("Qual è il titolo del libro?");
+string bookName = Console.ReadLine();
+
+
+Console.Write("Qual è l'autore?");
+string author = Console.ReadLine();
+
+
+Console.Write("Qual è il suo ISBN-10?");
+string ISBN10 = Console.ReadLine();
+
+
+Console.Write("Qual è il suo ISBN-13?");
+string ISBN13 = Console.ReadLine();
+
+
+Console.Write("Qual è il numero delle pagine del libro?");
+string pageNumberString = Console.ReadLine();
+int pageNumber = int.Parse(pageNumberString);
+
+
+Console.Write("Qual è il peso del libro?");
+string bookWeigthString = Console.ReadLine();
+int bookWeigth = int.Parse(bookWeigthString);
+
+
+Console.Write("Qual è l'altezza del libro?");
+string bookHeigthString = Console.ReadLine();
+float bookHeigth = float.Parse(bookHeigthString);
+
+
+Console.Write("Qual è la larghezza del libro?");
+string bookWidthString = Console.ReadLine();
+float bookWidth = float.Parse(bookWidthString);
+
+
+Console.Write("Qual è la profondità del libro?");
+string bookDepthString = Console.ReadLine();
+float bookDepth = float.Parse(bookDepthString);
+
+
+Console.Write("Qual è la valutazione media del libro?");
+string bookRatingString = Console.ReadLine();
+int bookRating = int.Parse(bookRatingString);
+
+
+Console.Write("Qual è il numero delle recensioni?");
+string bookValuesString = Console.ReadLine();
+int bookValues = int.Parse(bookValuesString);
+
+
+Console.Write("Il libro è disponibile in versione Kindle?");
+string kindleCheck = Console.ReadLine();
+
+
+Console.Write("Il libro è disponibile con copertina flessibile?");
+string flexCheck = Console.ReadLine();
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+
+
+
+
+Console.WriteLine("------ INFORMAZIONI LIBRO -----");
+Console.WriteLine("Titolo: " + bookName);
+Console.WriteLine("Autore: " + author);
+Console.WriteLine("ISBN-10: " + ISBN10);
+Console.WriteLine("ISBN-13: " + ISBN13);
+Console.WriteLine("Numero pagine: " + pageNumber);
+Console.WriteLine("Peso libro: " + bookWeigth + " g");
+Console.WriteLine("Dimensioni: " + bookHeigth + " x " + bookWidth + " x " + bookDepth + " cm ");
+Console.WriteLine("Valutazione: " + bookRating + " stelle ");
+Console.WriteLine("Numero recensioni: " + bookValues);
+Console.WriteLine("Disponibile in versione Kindle: " + kindleCheck);
+Console.WriteLine("Disponibile con copertina flessibile: " + flexCheck);
+Console.WriteLine("-------------------------------");
+
+
+
+
+
+
---
+++ BookStoreCSharp/BookStoreCSharp/Program.cs
@@ -0,0 +1,61 @@
+﻿string nomeLibro = "Le nuove barzellette su Totti";
+string author = "Francesco Totti";
+string ISBN10 = "8804524820";
+string ISBN13 = "978-8804524823";
+int pageNumber = 108;
+int bookWeigth = 150;
+float bookHeigth = 18.5f;
+float bookWidth = 11.5f;
+float bookDepth = 1.5f;
+int bookRating = 5;
+int bookValues = 9;
+bool kindleCheck = false;
+bool flexCheck = true;
+
+
+
+
+Console.WriteLine("------ INFORMAZIONI LIBRO -----");
+Console.WriteLine("Titolo: " + nomeLibro);
+Console.WriteLine("Autore: " + author);
+Console.WriteLine("ISBN-10: " + ISBN10);
+Console.WriteLine("ISBN-13: " + ISBN13);
+Console.WriteLine("Numero pagine: " + pageNumber);
+Console.WriteLine("Peso libro: " + bookWeigth + " g");
+Console.WriteLine("Dimensioni: " + bookHeigth + " x " + bookWidth + " x " + bookDepth + " cm ");
+Console.WriteLine("Valutazione: " + bookRating + " stelle ");
+Console.WriteLine("Numero recensioni: " + bookValues);
+
+
+
+if (kindleCheck == true)
+{
+    Console.WriteLine("Disponibile in versione Kindle: Sì");
+}
+else
+{
+    Console.WriteLine("Disponibile in versione Kindle: No");
+};
+
+
+
+if (flexCheck == true)
+{
+    Console.WriteLine("Disponibile con copertina flessibile: Sì");
+}
+else
+{
+    Console.WriteLine("Disponibile con copertina flessibile: No");
+};
+
+
+Console.WriteLine("-------------------------------");
+
+
+
+
+
+
+
+
+
